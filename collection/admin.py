14516979fe683a7314d56decb22733b9807c1635
Joe Cloughley <joecloughley@gmail.com>
2019-11-05 23:12:28 -0800

    Add static and dynamic content

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# import your model
+from collection.models import List
+
+# set up automated slug creation
+class ListAdmin(admin.ModelAdmin):
+    model = List
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(List, ListAdmin)
--- oiocha/urls.py
+++ oiocha/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-# our new import
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
-    # the below is new too!
     path('', views.index, name='home'),
+    # The new URL entries we're adding:
+    path('about/',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    path('contact/',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
+
     path('admin/', admin.site.urls),
 ]
