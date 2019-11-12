b7b746a73ed6bc5d1d7fb95adfb2e555a5c9ba82
Joe Cloughley <joecloughley@gmail.com>
2019-11-11 22:49:11 -0800

    Change app name

---
+++ hellowebapp/urls.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from collection import views
+from django.contrib.auth.views import(
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+from collection.backends import MyRegistrationView
+from django.views.generic import TemplateView, RedirectView
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/',TemplateView.as_view(template_name='about.html'),name='about'),
+    path('contact/',TemplateView.as_view(template_name='contact.html'),name='contact'),
+    path('lists/<slug>/', views.list_detail,name='list_detail'),
+    path('lists/<slug>/edit/', views.edit_list,name='edit_list'),
+    path('accounts/password/reset/',PasswordResetView.as_view(),name="password_reset"),
+    path('accounts/password/reset/done/',PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('accounts/password/done',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('accounts/register/',MyRegistrationView.as_view(),name='registration_register'),
+    path('accounts/create_list/', views.create_list,name='registration_create_list'),
+    path('accounts/',include('registration.backends.default.urls')),
+    path('admin/', admin.site.urls),
+    path('browse/name/',views.browse_by_name, name='browse'),
+    path('browse/name/<initial>/',views.browse_by_name, name='browse_by_name'),
+    path('browse/', RedirectView.as_view(pattern_name='browse', permanent=True)),
+    path('lists/', RedirectView.as_view(
+        pattern_name='browse', permanent=True)),
+    path('lists/<slug>/',
+        views.list_detail, name='list_detail'),
+    path('lists/<slug>/edit/',
+        views.edit_list, name='edit_list'),
+]
