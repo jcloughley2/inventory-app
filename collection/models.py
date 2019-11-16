e5ad5091227440ff2ef05bb825961d5665a6ad73
Joe Cloughley <joecloughley@gmail.com>
2019-11-16 12:27:26 -0800

    adjust list model to be owned by many users

--- collection/models.py
+++ collection/models.py
@@ -6,5 +6,5 @@ class List(models.Model):
     description = models.TextField()
     slug = models.SlugField(unique=True)
     # the new line we're adding
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
         blank=True, null=True)
